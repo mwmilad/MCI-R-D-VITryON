b51716424a69a54f05f14b00c2d09521bc4a8a75
Milad Seyfi <Milad.Seyfolahi@nak-mci.ir>
2026-02-08 22:54:07 +0330

    create custom dataloader on VTHON-HD dataset

--- ClothWarp/dataset.py
+++ ClothWarp/dataset.py
@@ -4,11 +4,22 @@ import torchvision
 from torch.utils.data import Dataset, DataLoader
 
 class ClothWarpingVVHD(Dataset):
-    def __init__(self, data_path) -> None:
+    """
+    Custom DataLoader for train Warping Module on VITHON-HD Dataset on data folder.
+    """
+    def __init__(self, data_path=r'ClothWarp\data\zalando-hd-resized\train') -> None:
         self.BASE_PATH = data_path
+        self. IMAGE_PATH = os.path.join(self.BASE_PATH, 'image')
+        print(self.IMAGE_PATH)
+
+    
+    def _test(self):
+        pass
     
     def __len__(self):
         pass
 
     def __getitem__(self, idx):
         pass
+
+dataloader = ClothWarpingVVHD()
